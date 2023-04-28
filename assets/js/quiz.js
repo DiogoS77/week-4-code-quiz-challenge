b8d2d31cfffcdd3d2dc7ca1b15ea6fb6ad23a041
DiogoS77 <diogo.messi.18@hotmail.com>
2023-04-28 18:29:50 +0100

    javascript code added, index.html changes

---
+++ assets/js/quiz.js
@@ -0,0 +1,124 @@
+// Get elements from the HTML
+var quizStart = document.getElementById("quizStart");
+var welcome = document.getElementById("Welcome");
+var quiz = document.getElementById("quiz");
+var result = document.getElementById("result");
+var options = document.getElementById("options");
+var message = document.getElementById("message");
+var timer = document.getElementById("timer");
+var summary = document.getElementById("summary");
+var initialsBox = document.getElementById("initials");
+var saveScoreBtn = document.getElementById("saveScore");
+var viewScoresBtn = document.getElementById("viewScores");
+var playAgainBtn = document.getElementById("playAgain");
+
+// Declare variables
+var secondsLeft = 0;
+var score = 0;
+var currentQuestion = 0;
+var countdownTimer;
+
+// Function to stop the game
+function stopGame() {
+  clearInterval(countdownTimer);
+  timer.textContent = "";
+  quiz.style.display = "none";
+  result.style.display = "flex";
+  summary.textContent = "Your Score is: " + score;
+  initialsBox.removeAttribute("hidden");
+  saveScoreBtn.removeAttribute("hidden");
+  viewScoresBtn.removeAttribute("hidden");
+  playAgainBtn.removeAttribute("hidden");
+}
+
+// Function to save score
+function onSaveScore(e) {
+  var initials = initialsBox.value;
+  if (initials !== "") {
+    localStorage.setItem(initials, score);
+    initialsBox.value = "";
+  }
+}
+
+// Function to view scores
+function onViewScores(e) {
+  window.location.href = "scores.html";
+}
+
+// Function to handle the user's answer
+function onSelectAnswer(e) {
+  var correctAnswer = questions[currentQuestion].answer;
+  var userAnswer = e.target.textContent;
+
+  if (correctAnswer === userAnswer) {
+    score++;
+    displayMessage("Correct!!!!!");
+  } else {
+    score--;
+    displayMessage("Wrong -_-");
+  }
+
+  displayQuestion();
+}
+
+// Function to display a message
+function displayMessage(msg) {
+  message.textContent = msg;
+
+  setTimeout(function () {
+    message.textContent = "";
+  }, 1000);
+}
+
+// Function to display a question
+function displayQuestion() {
+  currentQuestion++;
+
+  console.log("current question is" + currentQuestion);
+
+  if (currentQuestion >= questions.length) {
+    stopGame();
+    return;
+  }
+
+  var question = questions[currentQuestion];
+  document.getElementById("question").textContent = question.title;
+
+  options.innerHTML = "";
+
+  for (var i = 0; i < question.choices.length; i++) {
+    var option = document.createElement("div");
+    option.textContent = question.choices[i];
+    option.onclick = onSelectAnswer;
+    option.classList.add("option");
+
+    options.appendChild(option);
+  }
+}
+
+// Function to start the game
+function onStartGame() {
+  secondsLeft = 75;
+  currentQuestion = -1;
+  score = 0;
+
+  countdownTimer = setInterval(function () {
+    if (secondsLeft > 0) {
+      timer.textContent = secondsLeft;
+    } else {
+      stopGame();
+    }
+    secondsLeft--;
+  }, 1000);
+
+  welcome.style.display = "none";
+  result.style.display = "none";
+  quiz.style.display = "flex";
+
+  displayQuestion();
+}
+
+// Event listeners
+quizStart.addEventListener("click", onStartGame);
+saveScoreBtn.addEventListener("click", onSaveScore);
+viewScoresBtn.addEventListener("click", onViewScores);
