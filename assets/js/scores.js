b5ad6b59f3fd2a748012874115c7de61df690781
DiogoS77 <diogo.messi.18@hotmail.com>
2023-05-02 18:21:55 +0100

    fixed scoresjs to sort the scores from high to low

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -5,24 +5,32 @@ function onBackQuiz() {
   window.location.href = "index.html";
 }
 
+var scores = [];
 for (var i = 0; i < localStorage.length; i++) {
   var initials = localStorage.key(i);
   var score = localStorage.getItem(initials);
+  scores.push({ initials: initials, score: score });
+}
 
+scores.sort(function (a, b) {
+  return b.score - a.score;
+});
+
+scores.forEach(function (score) {
   var result = document.createElement("div");
   result.className = "result";
 
   var initialsDiv = document.createElement("div");
   initialsDiv.className = "score-item";
-  initialsDiv.textContent = initials;
+  initialsDiv.textContent = score.initials;
   result.appendChild(initialsDiv);
 
   var scoreDiv = document.createElement("div");
   scoreDiv.className = "score-item";
-  scoreDiv.textContent = score;
+  scoreDiv.textContent = score.score;
   result.appendChild(scoreDiv);
 
   scorespage.appendChild(result);
-}
+});
 
 backQuiz.addEventListener("click", onBackQuiz);
